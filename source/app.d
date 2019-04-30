cd0cc624a8f49f2ec285093aa6f323e3a2da1cae
Charles Gregory <charles.gregory@osumc.edu>
2019-04-30 14:10:03 -0400

    added bam reading: untested

--- source/app.d
+++ source/app.d
@@ -1,21 +1,35 @@
 import std.stdio;
 import hdf5.hdf5;
 import dhtslib.sam;
+import std.getopt;
+import std.array:array;
 import std.file:dirEntries,SpanMode;
-import std.algorithm:each;
+import std.algorithm:each,map;
 import std.algorithm.setops:setIntersection;
 
 string[] tmp_read_names;
 string[] filenames;
 ulong[string] read_names2fileindex;
+bool bam;
 void main(string[] args)
 {
-	// parse_fast5_names(args[1]);
+	GetoptResult res;
+	try {
+		res = getopt(args, std.getopt.config.bundling,
+		"bam|b","Input filtered read file is a bam file.",&bam);
+	} catch (GetOptException e){
+		stderr.writeln(e.msg);
+		stderr.writeln("-h or --help for usage.");
+	}
+	if(res.helpWanted){
+		defaultGetoptPrinter("fast5catcher usage: ./fast5catcher [options] [fast5 dir] [input bam/fastq] [output dir]",res.options);
+		return;
+	}
 	foreach(fn; dirEntries(args[1],SpanMode.depth)) {
 		parse_fast5_names(fn);
 	}
-	filenames.writeln;
-	read_names2fileindex.writeln;
+	tmp_read_names=setIntersection(read_names2fileindex.keys,parse_bam_reads(args[2])).array;
+	writeln(tmp_read_names);
 }
 
 void parse_fast5_names(string fn){
@@ -32,4 +46,8 @@ extern(C) static herr_t get_object_names(hid_t loc_id, const char *name, const H
 	tmp_read_names~=ZtoString(name);
     // writefln("\nName : %s", ZtoString(name));
     return 0;
+}
+
+string[] parse_bam_reads(string fn){
+	return SAMFile(fn).all_records.map!(x=>x.queryName.idup).array;
 }
