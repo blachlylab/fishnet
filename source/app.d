ffe60649f564a2860dc33401b86d8a8162d08d87
Charles Gregory <charles.gregory@osumc.edu>
2019-04-30 17:03:47 -0400

    It's alive! (working)

--- source/app.d
+++ source/app.d
@@ -6,17 +6,24 @@ import std.array:array;
 import std.file:dirEntries,SpanMode;
 import std.algorithm:each,map,sort;
 import std.algorithm.setops:setIntersection;
+import std.conv:to;
+import std.file:mkdirRecurse;
 
 string[] tmp_read_names;
 string[] filenames;
 ulong[string] read_names2fileindex;
 bool bam;
+int readlimit=4000;
+int readcount=0;
+hid_t outfile;
+int outfile_lab=0;
 void main(string[] args)
 {
 	GetoptResult res;
 	try {
 		res = getopt(args, std.getopt.config.bundling,
-		"bam|b","Input filtered read file is a bam file.",&bam);
+		"bam|b","Input filtered read file is a bam file.",&bam,
+		"readlimit|l","number of reads per output fast5.",&readlimit);
 	} catch (GetOptException e){
 		stderr.writeln(e.msg);
 		stderr.writeln("-h or --help for usage.");
@@ -24,12 +31,45 @@ void main(string[] args)
 	if(res.helpWanted){
 		defaultGetoptPrinter("fast5catcher usage: ./fast5catcher [options] [fast5 dir] [input bam/fastq] [output dir]",res.options);
 		return;
+	}else if(args.length!=4){
+		writeln("fast5catcher: Incorrect number of inputs!");
+		defaultGetoptPrinter("fast5catcher usage: ./fast5catcher [options] [fast5 dir] [input bam/fastq] [output dir]",res.options);
+		return;
 	}
+
+	// get all readnames from fast5s
 	foreach(fn; dirEntries(args[1],SpanMode.depth)) {
 		parse_fast5_names(fn);
 	}
+	writeln("parsed fast5 read names");
+	//intersect with bamfile read names
 	tmp_read_names=setIntersection(read_names2fileindex.keys.sort.array,parse_bam_reads(args[2])).array;
-	writeln(tmp_read_names);
+
+	writeln("intersected with filtered read name file");
+	//make output dir and first file
+	mkdirRecurse(args[3]);
+	outfile=H5F.create(args[3]~"/"~outfile_lab.to!string~".fast5",H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	read_names2fileindex.rehash;
+	tmp_read_names=tmp_read_names.sort!((a,b)=>read_names2fileindex[a]<read_names2fileindex[b]).array;
+	auto readfiles=tmp_read_names.map!(x=>filenames[read_names2fileindex[x]]);
+	// readfiles.writeln;
+	string prev;
+	hid_t infile=-1;
+	foreach(read;tmp_read_names){
+		if(prev!=readfiles.front){
+			if(infile!=-1){
+				H5F.close(infile);
+			}
+			infile=H5F.open(readfiles.front,H5F_ACC_RDONLY,H5P_DEFAULT);
+		}
+		outfile=verify_file_limit(outfile,args[3]);
+		copyReadData(read,infile,outfile);
+		prev=readfiles.front;
+		readfiles.popFront;
+		readcount++;
+	}
+
+	// writeln(tmp_read_names);
 }
 
 void parse_fast5_names(string fn){
@@ -50,4 +90,19 @@ extern(C) static herr_t get_object_names(hid_t loc_id, const char *name, const H
 
 string[] parse_bam_reads(string fn){
 	return SAMFile(fn).all_records.map!(x=>"read_"~x.queryName.idup).array.sort.array;
+}
+
+hid_t verify_file_limit(hid_t cur,string dir){
+	if(readcount>=readlimit){
+		H5F.close(outfile);
+		outfile_lab++;
+		cur=H5F.create(dir~"/"~outfile_lab.to!string~".fast5",H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+		readcount=0;
+		writeln("Opened a new file");
+	}
+	return cur;
+}
+
+void copyReadData(string read,hid_t from, hid_t to){
+	H5O.copy(from,"/"~read,to,"/"~read,H5P_DEFAULT,H5P_DEFAULT);
 }
